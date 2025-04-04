bf7913daa5334f01e7629d1494297096b9e7fa96
Alex Oliveira <alexoliveira7x@gmail.com>
2025-04-04 01:57:04 +0100

    improve: resistor color

--- resistor-color/resistor_color.c
+++ resistor-color/resistor_color.c
@@ -1,11 +1,12 @@
 #include "resistor_color.h"
 
+resistor_band_t band_colors[] = { BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE };
+
 int color_code(resistor_band_t color) {
     return color;
 }
 
 const resistor_band_t *colors(void)
 {
-    static const resistor_band_t colors[] = { BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE };
-    return colors;
+    return band_colors;
 }
