989fa4351cfa0e36d22d7cdd21b560703c6b6d03
Alex Oliveira <alexoliveira7x@gmail.com>
2025-04-06 18:17:27 +0100

    solve: Collatz Conjecture

--- collatz-conjecture/collatz_conjecture.c
+++ collatz-conjecture/collatz_conjecture.c
@@ -1,1 +1,21 @@
 #include "collatz_conjecture.h"
+
+int steps(int start) {
+    if (start < 1)
+        return ERROR_VALUE;
+    
+    int result = 0;
+    while (start != 1) {
+        // Bitwise AND
+        if (!(start & 1)) {
+            // Shift right
+            start = start >> 1;
+        } else {
+            start = (start * 3) + 1;
+        }
+
+        result += 1;
+    }
+
+    return result;
+}
