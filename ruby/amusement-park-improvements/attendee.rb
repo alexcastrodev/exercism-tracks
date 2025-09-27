dfe7e320fafb66348dd59303e22b9a2a7ae80e1e
Alexandro castro <alexandrocastro@mac.home>
2025-09-27 18:46:23 +0100

    start improvements

---
+++ ruby/amusement-park-improvements/attendee.rb
@@ -0,0 +1,25 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def height
+    @height
+  end
+
+  def pass_id
+    @pass_id
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  def has_pass?
+    !@pass_id.nil?
+  end
+end
